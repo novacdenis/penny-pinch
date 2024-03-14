563d13afceca39f0b9d3c7f00ef95c2fec1becde
Novac Denis <denisnovac2017@gmail.com>
2024-03-14 17:25:14 +0200

    Update transactions table columns

--- src/features/monitoring/components/transactions-table.tsx
+++ src/features/monitoring/components/transactions-table.tsx
@@ -2,12 +2,19 @@
 
 import type { Transaction } from "../types";
 import { ColumnDef } from "@tanstack/react-table";
+import dayjs from "dayjs";
 import { DataTable } from "@/components/ui/data-table";
 
 export const columns: ColumnDef<Transaction>[] = [
-  { accessorKey: "timestamp", header: "Date" },
+  {
+    header: "Date",
+    accessorFn: (row) => dayjs(row.timestamp).format("DD/MM/YYYY"),
+  },
   { accessorKey: "title", header: "Title" },
-  { accessorKey: "category", header: "Category" },
+  {
+    header: "Category",
+    accessorFn: (row) => row.category.slice(0, 1).toUpperCase() + row.category.slice(1),
+  },
   { accessorKey: "amount", header: "Amount" },
 ];
 
