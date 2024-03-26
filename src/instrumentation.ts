16408b6efdfdfc30a87647bf0df22dfc2c5c224d
cralex11 <45730710+cralex11@users.noreply.github.com>
2024-03-26 17:46:39 +0200

    fixup! refactoring db

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
+import dayjs from "dayjs";
 import * as customParseFormat from "dayjs/plugin/customParseFormat";
-
 export function register() {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
